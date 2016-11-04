cc0469567385e53bdaea3419a60bfeb2b22aa3eb
Caidler <Rafal.Szalkiewicz@gmail.com>
2016-11-04 10:19:16 +0100

    Update and rename code to code.py
    wstęp do inputa

---
+++ code.py
@@ -0,0 +1,26 @@
+for liczba in range(1,10):
+	print liczba
+
+print 'Witaj w Pythonie!'
+print 'Witaj w Pythonie ponownie!'.upper()
+
+def mojaFunkcja(liczba):
+	return liczba*2
+
+x = 2
+print mojaFunkcja(x)
+
+czasownik = ['Sprzątnij', "Posprzątaj", "Pozbieraj" "Wyczyść", "Wyczysc", "Zbierz", "Wyczysc", "Wyczyść", "Sprzatnij", "Posprzataj"];
+rzeczownik = ['Okolice', "Okolice Ulicy", "Okoliceulicy", "Wszystko", "Kazdaulice", "Kazda ulice", "Kazdą ulicę", "Ulice", "ulice", "śmieci", "smieci"];
+ulica = ["Umultowska", "Szybka", "Wredna", "Nowoczesna"];
+while True:
+    print(czasownik);
+    print(rzeczownik);
+    print(ulica);
+    print("Śmieciarka czeka na Twoje polecenie : ");
+    zdanie = input();
+    for wyraz in rzeczownik
+        if zdanie == wyraz:
+            print("Podałeś rzeczownik!");
+            
+print("Koniec!")
